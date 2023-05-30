628b4680a67fe3b47f7e8e69b4f04ec5f6e65ec4
root <root@ip-172-31-19-211.ap-south-1.compute.internal>
2023-05-30 09:08:06 +0000

    add changes

--- keycloak/script.sh
+++ keycloak/script.sh
@@ -5,3 +5,25 @@ docker-compose up -d
 sleep 5
 
 docker exec local_keycloak /opt/jboss/keycloak/bin/add-user-keycloak.sh -u admin -p admin && docker restart local_keycloak
+
+
+sleep 30
+
+docker exec local_keycloak /opt/jboss/keycloak/bin/kcadm.sh config credentials --server  http://local_keycloak:8080/auth  --realm master --user admin --password admin
+
+docker exec local_keycloak /opt/jboss/keycloak/bin/kcadm.sh create realms --set "realm=sunbird-rc"  --set "enabled=true"
+
+docker exec local_keycloak /opt/jboss/keycloak/bin/add-user-keycloak.sh  -r sunbird-rc -u admin-app -p admin  && docker restart local_keycloak
+
+sleep 30
+
+docker exec local_keycloak /opt/jboss/keycloak/bin/kcreg.sh config credentials --server  http://local_keycloak:8080/auth  --realm sunbird-rc --user admin-app --password admin
+
+#docker exec local_keycloak /opt/jboss/keycloak/bin/kcreg.sh create -s clientId=admin-api
+
+docker exec local_keycloak /opt/jboss/keycloak/bin/kcreg.sh create -s clientId=registry-frontend
+
+docker exec local_keycloak /opt/jboss/keycloak/bin/kcadm.sh config credentials --server  http://local_keycloak:8080/auth  --realm master --user admin --password admin
+
+docker exec local_keycloak /opt/jboss/keycloak/bin/kcadm.sh create clients -r sunbird-rc -s clientId=admin-api -s enabled=true -s clientAuthenticatorType=client-secret -s secret=d0b8122f-8dfb-46b7-b68a-f5cc4e25d000
+
